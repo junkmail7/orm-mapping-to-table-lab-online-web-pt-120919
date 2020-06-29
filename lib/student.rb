561cc5808f8e3d7b08f200358afcdf510a101201
William Pannell <lpann5@gmail.com>
2020-06-29 22:55:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,24 @@
 class Student
+  attr_accessor :grade, :name, :idea
+
+  def initialize(grade, name, id = nil)
+    @grade = grade
+    @name = name
+    @id = id
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
